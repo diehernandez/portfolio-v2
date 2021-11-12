a2b371fbb8a35b3003bd2cca4fa6113dc256a3ca
diehernandez <dhernandezca@ripley.com>
2021-11-12 20:49:10 -0300

    style: add profile image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} aling="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500','whiteAlpha.200')} p={3} mb={6} aling="center">
                 Hello, I&apos;m a full-stack devoloper based in Chile
             </Box>
             <Box display={{ md: 'flex' }}>
@@ -12,6 +12,9 @@ const Page = () => {
                         Diego Hernandez
                     </Heading>
                     <p> A system builder </p>
+                </Box>
+                <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{md: 6}} align="center">
+                    <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="120px" display="inline-block" borderRadius="full" src="/images/me.jpg" alt="Profile Image"/>
                 </Box>
             </Box>
         </Container>
