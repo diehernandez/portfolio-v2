c0d679280b11166c6ff897fe088e6d5f53b9da63
diehernandez <dhernandezca@ripley.com>
2021-11-12 21:06:25 -0300

    feat: add animated profile sections

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
-
+import Section from '../components/section'
 const Page = () => {
     return (
         <Container>
@@ -14,9 +14,15 @@ const Page = () => {
                     <p> A system builder </p>
                 </Box>
                 <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{md: 6}} align="center">
-                    <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="120px" display="inline-block" borderRadius="full" src="/images/me.jpg" alt="Profile Image"/>
+                    <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="full" src="/images/me.jpg" alt="Profile Image"/>
                 </Box>
             </Box>
+            <Section delay={0.4}>
+                <Heading as="h3" variant="section-title">
+                    Work
+                </Heading>
+                <p>Paragraph</p>
+            </Section>
         </Container>
     )
 }
